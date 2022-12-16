d9d654d8f55f7df6885a71332fad0b3db23f522b
Lahvich-Efim <120374125+Lahvich-Efim@users.noreply.github.com>
2022-12-16 22:04:23 +0300

    Add files via upload

---
+++ game_machine.cpp
@@ -0,0 +1,147 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <ctime>
+#include <io.h>
+#include <fcntl.h>
+#include "settings.cpp"
+
+using namespace  std;
+
+extern int chance;
+extern float balance;
+
+int f1(int n, int psize_cimbol)
+{
+	if (n == psize_cimbol)
+		return (n -= psize_cimbol);
+	return n;
+
+}
+int f2(int n, int psize_column, int psize_cimbol)
+{
+		if (n - psize_column < 0)
+			return (n = psize_cimbol - psize_column + n + 1);
+		else
+			return ( n -= psize_column - 1);
+}
+int f3(int pr, int psize_column, int psize_cimbol)
+{
+	pr -= (psize_column - 1) / 2;
+	if (pr < 0)
+		pr += psize_cimbol;
+	return(pr);
+}
+
+int main()
+{
+	wchar_t B[] = { L'A', L'$', L'€', L'♛', L'⚜', L'☺', L'7', L'߷', L'♠', L'\0' };
+	int r, m, p1 = 1, p2 = 1, p3 = 1, chance_rand, size_column = 5, r1, r2, r3, size_cimbol = sizeof(B) / 2 - 1, stop = 200;
+	srand((unsigned)time(NULL));
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	_setmode(_fileno(stdin), _O_U16TEXT);
+	_setmode(_fileno(stderr), _O_U16TEXT);
+	r1 = rand() % size_cimbol;
+	r2 = rand() % size_cimbol;
+	r3 = rand() % size_cimbol;
+	chance_rand = rand() % 10000 + 1;
+	if(chance_rand <= chance)
+	{
+		r = rand() % 9 - 4;
+		r1 = 1 + r;
+		r2 = 3 + r;
+		r3 = 5 + r;
+	}
+	for (int j = 0; j < size_column; j++)
+	{
+		wcout << L"  " << B[j];
+		wcout << L"  " << B[j];
+		wcout << L"  " << B[j];
+		wcout << endl;
+	}
+	for (int j = 0; j < size_column; j++)
+	{
+		wcout << "\x1b[A";
+	}
+	for (int i = 1; i < 350; i++)
+	{
+
+		if (i == 150)
+		{
+			p1 = f3(r1, size_column, size_cimbol);
+			p2 = f3(r2, size_column, size_cimbol);
+			p3 = f3(r3, size_column, size_cimbol);
+		}
+		for(int j = 0; j < size_column; j++)
+		{
+			p1 = f1(p1, size_cimbol);
+			p2 = f1(p2, size_cimbol);
+			p3 = f1(p3, size_cimbol);
+			if (i > 30 && i < (stop + 6 * size_cimbol + 4))
+				wcout << L"  " << B[p1++];
+			if (i > (30 + 2 * size_cimbol + 5) && i < (stop + 3 * size_cimbol + 2))
+				wcout << L"  " << B[p2++];
+			if (i > (30 + 4 * size_cimbol + 11) && i < stop)
+				wcout << L"  " << B[p3++];
+			if(i > 30 && i < (stop + 6 * size_cimbol + 4))
+				wcout << endl;
+		}
+		if (i > 30 && i < (stop + 6 * size_cimbol + 4))
+		{
+			p1 = f2(p1, size_column, size_cimbol);
+		}
+		if (i > (30 + 2 * size_cimbol + 5) && i < (stop + 3 * size_cimbol + 2))
+		{
+			p2 = f2(p2, size_column, size_cimbol);
+		}
+		if (i > (30 + 4 * size_cimbol + 11) && i < stop)
+		{
+			p3 = f2(p3, size_column, size_cimbol);
+		}
+		if(i > 30 && i < (stop + 6 * size_cimbol + 4))
+		for (int j = 0; j < size_column; j++)
+		{
+			wcout << "\x1b[A";
+		}
+		Sleep(30);
+	}
+	for (int j = 0; j < size_column; j++)
+	{
+		wcout << endl;
+	}
+	if (p1 == p2 && p3 == p2)
+	{
+		if (p1 == 5)
+		{
+			wcout << L"Вы выграли!!!\nТри семерки. Отлично!!!";
+			m = 7;
+		}
+		if (p1 == 2)
+		{
+			wcout << L"ПОЗДРАВЛЯЮ!!!\nУ вас выпало КОРОНА!!!(вирус)";
+			m = 10;
+		}
+		if (p1 == 1 || p1 == 0)
+		{
+			wcout << L"Вы выиграли!!!\n";
+			m = 3;
+		}
+		if (p1 == 3 || p1 == 7)
+		{
+			wcout << L"Вы выиграли!!!\n";
+			m = 5;
+		}
+		if (p1 == 8 || p1 == 6 || p1 == 4)
+		{
+			wcout << L"Вы выиграли!!!\n";
+			m = 2;
+		}
+		wcout << L"Вы получаете " << balance * m;
+		return (balance*m);
+	}
+
+	else
+	{
+		wcout << L"Вы проиграли.";
+		return (-balance);
+	}
+}
