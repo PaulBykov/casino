b572b516f37db7e289f493bd823b169ae283ef09
Lahvich-Efim <120374125+Lahvich-Efim@users.noreply.github.com>
2022-12-25 22:35:42 +0300

    Add files via upload

--- blackjack.cpp
+++ blackjack.cpp
@@ -6,32 +6,35 @@
 #include <thread>
 #include <map>
 #include <iomanip>
+#include <io.h>
+#include <fcntl.h>
+#include <Windows.h>
 
 extern float balance;
 extern float bet;
 
 using namespace std;
 
-inline void GetCard(int& hand, vector< vector<string> >& coloda, vector< vector<string> >& player, int& temp);
+inline void GetCard(int& hand, vector< vector<wstring> >& coloda, vector< vector<wstring> >& player, int& temp);
 
 
-inline int GetSum(vector< vector<string> >& player) {
+inline int GetSum(vector< vector<wstring> >& player) {
 	int sum = 0;
-	map<string, int> point; // дешифратор: величина карты = кол-во очков
+	map<wstring, int> point; // дешифратор: величина карты = кол-во очков
 
-	point["2"] = 2;
-	point["3"] = 3;
-	point["4"] = 4;
-	point["5"] = 5;
-	point["6"] = 6;
-	point["7"] = 7;
-	point["8"] = 8;
-	point["9"] = 9;
-	point["10"] = 10;
-	point["J"] = 10;
-	point["Q"] = 10;
-	point["K"] = 10;
-	point["A"] = 11;
+	point[L"2"] = 2;
+	point[L"3"] = 3;
+	point[L"4"] = 4;
+	point[L"5"] = 5;
+	point[L"6"] = 6;
+	point[L"7"] = 7;
+	point[L"8"] = 8;
+	point[L"9"] = 9;
+	point[L"10"] = 10;
+	point[L"J"] = 10;
+	point[L"Q"] = 10;
+	point[L"K"] = 10;
+	point[L"A"] = 11;
 	for (int i = 0; i < player.size(); i++) {
 		
 		sum += point[player[i][0]];
@@ -45,15 +48,18 @@ inline int GetSum(vector< vector<string> >& player) {
 inline void jack() {
 	setlocale(LC_ALL, "Rus");
 	const int n = 52;
+	
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	_setmode(_fileno(stdin), _O_U16TEXT);
+	_setmode(_fileno(stderr), _O_U16TEXT);
 
+	vector< vector<wstring> > coloda;
+	coloda.resize(n, vector <wstring>(2));
+	vector<wstring> cards = {L"2", L"3", L"4", L"5", L"6", L"7", L"8", L"9", L"10", L"J", L"Q", L"K", L"A"};
+	vector<wstring> mast = { L"♠", L"♥", L"♣", L"♦" }; //{ "♠", "♥", "♣", "♦" };
 
-	vector< vector<string> > coloda;
-	coloda.resize(n, vector <string>(2));
-	vector<string> cards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-	vector<string> mast = { "#", "$", "%", "&" }; //{ "♠", "♥", "♣", "♦" };
-
-	vector< vector<string> > diller;
-	diller.resize(11, vector <string>(2, "0"));
+	vector< vector<wstring> > diller;
+	diller.resize(11, vector <wstring>(2, L"0"));
 
 	srand((unsigned)time(NULL));
 	int i = 0, j = 2;
@@ -67,41 +73,41 @@ inline void jack() {
 	GetCard(hand_diller, coloda, diller, temp);
 	GetCard(hand_diller, coloda, diller, temp);
 
-	vector< vector<string> > player;
-	player.resize(11, vector <string>(2, "0"));
+	vector< vector<wstring> > player;
+	player.resize(11, vector <wstring>(2, L"0"));
 
-	do { temp = rand() % 52; } while (coloda[temp][0] == "0");
+	do { temp = rand() % 52; } while (coloda[temp][0] == L"0");
 	int hand = 0;
 	GetCard(hand, coloda, player, temp);
 	GetCard(hand, coloda, player, temp);
 	
 	tp:
 	while (GetSum(player) < 21) {
-		cout << "Ваши карты: " << endl;
+		wcout << L"Ваши карты: " << endl;
 		i = 0;
-		while (player[i][0] != "0") {
-			char symbol = '-', space = ' ';
-			cout << " " << setw(9) << setfill(symbol) << symbol << endl;
-			cout << "|";
-			if (player[i][0] == "10") {
-				cout << setw(5) << setfill(space) << space;
+		while (player[i][0] != L"0") {
+			wchar_t symbol = L'-', space = L' ';
+			wcout << L" " << setw(9) << setfill(symbol) << symbol << endl;
+			wcout << L"|";
+			if (player[i][0] == L"10") {
+				wcout << setw(5) << setfill(space) << space;
 			}
 			else {
-				cout << setw(6) << setfill(space) << space;
+				wcout << setw(6) << setfill(space) << space;
 			}
-			cout << player[i][0] << " " << player[i][1] << "|" << endl;
+			wcout << player[i][0] << L" " << player[i][1] << L"|" << endl;
 			for (int j = 0; j < 6; j++) {
-				cout << "|";
-				cout << setw(9) << setfill(space) << space << "|" << endl;
+				wcout << L"|";
+				wcout << setw(9) << setfill(space) << space << L"|" << endl;
 			}
-			cout << " " << setw(9) << setfill(symbol) << symbol << endl;
+			wcout << L" " << setw(9) << setfill(symbol) << symbol << endl;
 
 			i++;
 		}
-		cout << "Ваши очки: " << GetSum(player) << endl;
-		cout << "Желаете взять ещё карту? (1 - да, 2 - нет)" << endl;
+		wcout << L"Ваши очки: " << GetSum(player) << endl;
+		wcout << L"Желаете взять ещё карту? (1 - да, 2 - нет)" << endl;
 		int decision = 0;
-		cin >> decision;
+		wcin >> decision;
 		system("cls");
 		switch (decision) {
 			case(1):{
@@ -120,93 +126,93 @@ inline void jack() {
 
 
 	if (GetSum(player) <= 21) {
-		cout << "Ваши карты: " << endl;
+		wcout << L"Ваши карты: " << endl;
 		i = 0;
-		while (player[i][0] != "0") {
-			char symbol = '-', space = ' ';
-			cout << " " << setw(9) << setfill(symbol) << symbol << endl;
-			cout << "|";
-			if (player[i][0] == "10") {
-				cout << setw(5) << setfill(space) << space;
+		while (player[i][0] != L"0") {
+			wchar_t symbol = L'-', space = L' ';
+			wcout << L" " << setw(9) << setfill(symbol) << symbol << endl;
+			wcout << L"|";
+			if (player[i][0] == L"10") {
+				wcout << setw(5) << setfill(space) << space;
 			}
 			else {
-				cout << setw(6) << setfill(space) << space;
+				wcout << setw(6) << setfill(space) << space;
 			}
-			cout << player[i][0] << " " << player[i][1] << "|" << endl;
+			wcout << player[i][0] << " " << player[i][1] << L"|" << endl;
 			for (int j = 0; j < 6; j++) {
-				cout << "|";
-				cout << setw(9) << setfill(space) << space << "|" << endl;
+				wcout << L"|";
+				wcout << setw(9) << setfill(space) << space << L"|" << endl;
 			}
-			cout << " "  << setw(9) << setfill(symbol) << symbol << endl;
+			wcout << L" "  << setw(9) << setfill(symbol) << symbol << endl;
 
 			i++;
 		}
-		cout << "Ваши очки: " << GetSum(player) << endl;
+		wcout << L"Ваши очки: " << GetSum(player) << endl;
 		while (GetSum(diller) <= 18 && GetSum(diller) < GetSum(player)) {
 			GetCard(hand_diller, coloda, diller, temp);
 		}
 
-		cout << "Карты Диллера: " << endl;
+		wcout << L"Карты Диллера: " << endl;
 		i = 0;
-		while (diller[i][0] != "0") {
-			char symbol = '-', space = ' ';
-			cout << " " << setw(9) << setfill(symbol) << symbol << endl;
-			cout << "|";
-			if (player[i][0] == "10") {
-				cout << setw(5) << setfill(space) << space;
+		while (diller[i][0] != L"0") {
+			wchar_t symbol = L'-', space = L' ';
+			wcout << L" " << setw(9) << setfill(symbol) << symbol << endl;
+			wcout << L"|";
+			if (player[i][0] == L"10") {
+				wcout << setw(5) << setfill(space) << space;
 			}
 			else {
-				cout << setw(6) << setfill(space) << space;
+				wcout << setw(6) << setfill(space) << space;
 			}
-			cout << diller[i][0] << " " << diller[i][1] << "|" << endl;
+			wcout << diller[i][0] << L" " << diller[i][1] << L"|" << endl;
 			for (int j = 0; j < 6; j++) {
-				cout << "|";
-				cout << setw(9) << setfill(space) << space << "|" << endl;
+				wcout << L"|";
+				wcout << setw(9) << setfill(space) << space << L"|" << endl;
 			}
-			cout << " " << setw(9) << setfill(symbol) << symbol << endl;
+			wcout << L" " << setw(9) << setfill(symbol) << symbol << endl;
 
 			i++;
 		}
 
-		cout << "Сумма диллера: " << GetSum(diller) << endl;
+		wcout << L"Сумма диллера: " << GetSum(diller) << endl;
 
 
 		if (GetSum(diller) > GetSum(player) && GetSum(diller) < 22) {
-			cout << "Победа диллера!" << endl;
+			wcout << L"Победа диллера!" << endl;
 			balance -= bet;
 		}
 		else if (GetSum(diller) == GetSum(player) && GetSum(diller) < 22) {
-			cout << "Ничья!" << endl;
+			wcout << L"Ничья!" << endl;
 		}
 		else {
-			cout << "Вы победили!" << endl;
+			wcout << L"Вы победили!" << endl;
 			balance += bet;
 		}
 	}
 	else {
-		cout << "Ваши карты: " << endl;
+		wcout << L"Ваши карты: " << endl;
 		i = 0;
-		while (player[i][0] != "0") {
-			char symbol = '-', space = ' ';
-			cout << " " << setw(9) << setfill(symbol) << symbol << endl;
-			cout << "|";
-			if (player[i][0] == "10") {
-				cout << setw(5) << setfill(space) << space;
+		while (player[i][0] != L"0") {
+			wchar_t symbol = L'-', space = L' ';
+			wcout << L" " << setw(9) << setfill(symbol) << symbol << endl;
+			wcout << L"|";
+			if (player[i][0] == L"10") {
+				wcout << setw(5) << setfill(space) << space;
 			}
 			else {
-				cout << setw(6) << setfill(space) << space;
+				wcout << setw(6) << setfill(space) << space;
 			}
-			cout << player[i][0] << " " << player[i][1] << "|" << endl;
+			wcout << player[i][0] << L" " << player[i][1] << L"|" << endl;
 			for (int j = 0; j < 6; j++) {
-				cout << "|";
-				cout << setw(9) << setfill(space) << space << "|" << endl;
+				wcout << L"|";
+				wcout << setw(9) << setfill(space) << space << L"|" << endl;
 			}
-			cout << setw(9) << setfill(symbol) << symbol << endl;
+			wcout << setw(9) << setfill(symbol) << symbol << endl;
 
 			i++;
 		}
-		cout << "Ваши очки: " << GetSum(player) << endl;
-		cout << "Вы проиграли!" << endl;
+		wcout << L"Ваши очки: " << GetSum(player) << endl;
+		wcout << L"Вы проиграли!" << endl;
 		balance -= bet;
 	}
 
@@ -214,11 +220,11 @@ inline void jack() {
 	this_thread::sleep_for(std::chrono::milliseconds(10000));
 }
 
-inline void GetCard(int& hand, vector< vector<string> >& coloda, vector< vector<string> >& player, int& temp) {
+inline void GetCard(int& hand, vector< vector<wstring> >& coloda, vector< vector<wstring> >& player, int& temp) {
 	// hand -  кол-во карт в руке
-	do { temp = rand() % 52; } while (coloda[temp][0] == "0"); // генерируем новую карту, пока не встретится та, что еще есть в колоде 
+	do { temp = rand() % 52; } while (coloda[temp][0] == L"0"); // генерируем новую карту, пока не встретится та, что еще есть в колоде 
 	player[hand][0] = coloda[temp][0];
 	player[hand][1] = coloda[temp][1];
-	coloda[temp][0] = "0";
+	coloda[temp][0] = L"0";
 	hand++;
 }
